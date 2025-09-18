617422a0861d00bf3a27160feafed98e23bcedec
Lucas Anthony Carvalho Brito <lucasath@hotmail.com>
2025-09-18 19:32:03 -0300

    feat: handdle de errors

---
+++ src/main/java/com/example/amltest/config/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.amltest.config;
+
+import com.example.amltest.dto.error.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
